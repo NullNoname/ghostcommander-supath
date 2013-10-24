64340e279c5dd140b1b4aa23bed486b9c6562e7d
NullNoname <pbomqlu910963@gmail.com>
2013-10-24 21:52:26 +0900

    Fixed a bug that chmod/chown doesn't obey user's busybox path setting

--- src/com/ghostsq/commander/root/EditRootPermissions.java
+++ src/com/ghostsq/commander/root/EditRootPermissions.java
@@ -1,10 +1,13 @@
 package com.ghostsq.commander.root;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 import com.ghostsq.commander.utils.EditPermissions;
 import com.ghostsq.commander.utils.Permissions;
 
 public class EditRootPermissions extends EditPermissions {
-   
+
     @Override
     protected void apply( Permissions np ) {
         String cmd = null;
@@ -17,13 +20,18 @@ public class EditRootPermissions extends EditPermissions {
         if( a != null && a.length() > 0 ) {
             a.append( " '" ).append( file_path ).append( "'" );
             if( cmd == null ) cmd = "";
-            cmd += "busybox chmod " + a.toString();
+            cmd += getBusyBoxPath() + "chmod " + a.toString();
         }
         if( cmd != null ) {
             ExecEngine ee = new ExecEngine( this, null, cmd, false, 500 );
             ee.setHandler( new EditPermissions.DoneHandler() );
             ee.start();
         }
+    }
+
+    public final String getBusyBoxPath() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences( this );
+        return sharedPref.getString( "busybox_path", "busybox" ) + " ";
     }
 }
 
